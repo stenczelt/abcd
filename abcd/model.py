a2cf6f561e0e9dccef77fd712114c90befe18b41
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2023-08-24 15:14:59 +0000

    Fix formatting for non-string value error

--- abcd/model.py
+++ abcd/model.py
@@ -43,7 +43,7 @@ class Hasher(object):
             self.method.update(value)
 
         else:
-            raise ValueError("The {} type cannot be hashed! (Value: {})", format(type(value), value))
+            raise ValueError(f"The {type(value)} type cannot be hashed! (Value: {value})")
 
     def __call__(self):
         """Retrieve the digest of the hash."""
